34dbe876225a2c09686118ee7f6309f341383e78
unknown <gamejam@D34-25.bellecour.local>
2018-01-27 00:59:27 +0100

    puyoOnGround

---
+++ PuyoOnGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuyoOnGround : MonoBehaviour {
+
+	private List<GameObject> puyoOnGroundList;
+	
+	// Use this for initialization
+	void Start () {
+		puyoOnGroundList = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	List<GameObject> getOnGroundList(List<GameObject> puyoList)
+	{
+		foreach (var puyo in puyoList)
+		{
+			if (puyo.GetComponent<Puyo>().isOnGround)
+				puyoOnGroundList.Add(puyo);
+		}
+
+		return puyoOnGroundList;
+	}
+}
