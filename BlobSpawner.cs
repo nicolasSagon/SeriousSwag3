a0919813763c95ef14665fb96f2dec166a420e0b
unknown <gamejam@D34-25.bellecour.local>
2018-01-26 23:36:26 +0100

    spawner et clavier

---
+++ BlobSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobSpawner : MonoBehaviour {
+
+	public float timeLeftBeforeSpawn;
+	public float timeBetweenSpawn;
+
+	List<GameObject> blobList;
+
+	// Use this for initialization
+	void Start () {
+		blobList = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeLeftBeforeSpawn -= Time.deltaTime;
+		if (timeLeftBeforeSpawn <= 0) {
+			GameObject blob = new GameObject ();
+			blobList.Add (blob);
+			timeLeftBeforeSpawn = timeBetweenSpawn;
+		}
+		//Debug.Log(blobList.Count.ToString());
+	}
+}
---
+++ KeyboardPress.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardPress : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (char c in Input.inputString) {
+			if (Input.anyKeyDown && c >='a' && c <='z') {
+				Debug.Log(c);
+			}
+		}
+	}
+}
